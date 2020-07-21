823e0398276011c20c7282ede2fe20431a9a1aac
Daniel Pavela <daniel.pavela@gmail.com>
2020-07-21 14:04:03 +1000

    Add work-in-progress pause menu

---
+++ Scripts/UI/pauseMenu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		# position.x = get_node("../player").position.x
+		# position.y = get_node("../player").position.y
+		get_tree().paused = not get_tree().paused
+		visible = not visible
+
+
+func _on_resume_pressed():
+	get_tree().paused = false
+	visible = false
+
+
+func _on_returnTitle_pressed():
+	get_tree().paused = false
+	visible = false
+	get_tree().change_scene("res://Scenes/titleScreen.tscn")
